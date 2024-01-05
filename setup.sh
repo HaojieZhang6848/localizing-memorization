22045e0d32854e538fcea13fd01ff167ea984a33
Haojie Zhang <zhj630985214@gmail.com>
2024-01-06 00:42:41 +0800

    环境

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ conda activate mem_loc
 conda install pytorch torchvision pytorch-cuda=11.8 -c pytorch -c nvidia -y
 pip install ipdb matplotlib lightning-bolts
 pip uninstall pytorch-lightning-bolts pytorch_lightning==1.4.9
+pip install lightning-bolts==0.7.0 pytorch_lightning==1.9.5 matplotlib==3.8.2 seaborn==0.13.1
